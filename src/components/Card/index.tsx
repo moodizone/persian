4a247fbed73df7cf2d677311342c8bc653344d71
moodi <moodizone@gmail.com>
2025-04-07 20:03:42 +0330

    🚧 add card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { twMerge } from "tailwind-merge";
+
+function Card({
+  children,
+  className,
+  ...others
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>) {
+  return (
+    <div
+      {...others}
+      className={twMerge(
+        "bg-white max-w-[1200px] mx-auto px-4 md:px-9 py-8 md:py-10 rounded-lg border border-mercury",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Card;
